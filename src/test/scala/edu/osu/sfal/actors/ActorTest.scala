33f6afea059e3afd47818b75086f3410fe2ca897
Aaron Malone <aaronmalone@gmail.com>
2014-04-02 01:55:03 -0400

    Adding base for actor test classes

---
+++ src/test/scala/edu/osu/sfal/actors/ActorTest.scala
@@ -0,0 +1,15 @@
+package edu.osu.sfal.actors
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{WordSpecLike, BeforeAndAfterAll}
+
+class ActorTest(val actorSystem: ActorSystem)
+  extends TestKit(actorSystem)
+  with ImplicitSender
+  with WordSpecLike
+  with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("actorTest"))
+
+}
