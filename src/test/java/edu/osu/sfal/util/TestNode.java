3b68ebb9b653d72705c6226096e3b7e480820c38
Aaron Malone <aaronmalone@gmail.com>
2014-05-07 00:31:24 -0400

    Minor changes to TestNode.

--- src/test/java/edu/osu/sfal/util/TestNode.java
+++ src/test/java/edu/osu/sfal/util/TestNode.java
@@ -7,9 +7,12 @@ import edu.osu.lapis.util.Sleep;
 public class TestNode {
 	public static void main(String[] args) {
 		LapisApi lapisApi = new LapisApi("test1", "http://localhost:8910", "http://localhost:8777");
+
 		lapisApi.publishReadOnly("SIMULATION_FUNCTION_NAME", "test.1");
+
 		double[] readyToCalculate = Flags.getFlag(false);
 		lapisApi.publish("readyToCalculate", readyToCalculate);
+
 		double[] finishedCalculating = Flags.getFlag(true);
 		lapisApi.publishReadOnly("finishedCalculating", finishedCalculating);
 
@@ -20,8 +23,7 @@ public class TestNode {
 
 		while(true) {
 			while(!Flags.evaluateFlagValue(readyToCalculate)) {
-				System.out.println();
-				Sleep.sleep(2000);
+				Sleep.sleep(200);
 			}
 			Flags.setFlagFalse(finishedCalculating);
 			Flags.setFlagFalse(readyToCalculate);
